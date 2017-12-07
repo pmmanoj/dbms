29ef267bfe3a44cb83f219ba1882c38f74fd679a
pmmanoj <pmanoj@ibdsys.com>
2017-12-06 22:21:39 -0600

    HelloWorld.groovy first compiled class

---
+++ src/main/java/com/evds/dcim/HelloWorld.groovy
@@ -0,0 +1,9 @@
+package com.evds.dcim
+
+/**
+ * Created by sanjivsingh on 12/6/17.
+ */
+
+println "Hello, World -- Groovy 2.4.12"
+println "Using Maven via Artifactory and reaching Maven Central"
+println "Bye . . . "
